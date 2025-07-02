e2d4ffefed67c7a9d67f2c8fa371a6a8cbe551e3
Shavernev Oleg <oleg.shavernev1993@gmail.com>
2025-07-02 17:27:13 +0300

    fexed feed-sync script

--- greenbone/files/opt/greenbone-community-edition/feed-sync.sh
+++ greenbone/files/opt/greenbone-community-edition/feed-sync.sh
@@ -7,4 +7,7 @@ trap 'echo "$(date) feed synchronization failure"; exit 1' ERR
 docker compose --file /opt/greenbone-community-edition/docker-compose.yaml --project-name greenbone-community-edition \
     up --pull always --detach notus-data vulnerability-tests scap-data dfn-cert-data cert-bund-data report-formats data-objects
 
+docker compose --file /opt/greenbone-community-edition/docker-compose.yaml --project-name greenbone-community-edition \
+    restart
+
 echo "$(date) feed synchronization succeeded"
